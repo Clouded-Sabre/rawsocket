1d5c0db951ff3edc8988714b7f78e2734821df9a
Clouded-Sabre <rodgerwang23@gmail.com>
2025-04-25 21:57:26 +0800

    - refine debug info in pcapsession.go

--- lib/pcapsession.go
+++ lib/pcapsession.go
@@ -192,7 +192,7 @@ func (ps *pcapSession) handleIncomingPackets() {
 
 // processPacket processes an incoming packet and forwards it to the appropriate RawIPConn
 func (ps *pcapSession) processIncomingPacket(packet *gopacket.Packet) {
-	log.Println("pcapSession.processIncomingPacket: start processing packet.")
+	log.Printf("pcapSession.processIncomingPacket(%s): start processing packet.\n", ps.params.iface.Name)
 	startTime := time.Now() // Start timing
 
 	// Extract the IPv4 layer
