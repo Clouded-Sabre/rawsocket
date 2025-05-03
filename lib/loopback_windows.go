0a73c0ef64615ebf47daeb8dc81ff7d2eb3de49f
Clouded-Sabre <rodgerwang23@gmail.com>
2025-05-03 09:21:21 +0800

    - fix windows loopback encapsulation

--- lib/loopback_windows.go
+++ lib/loopback_windows.go
@@ -3,11 +3,44 @@
 
 package lib
 
-import "github.com/google/gopacket"
+import (
+	"encoding/binary"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+// WindowsLoopbackHeader represents the 4-byte header used in Windows loopback packets
+type WindowsLoopbackHeader struct {
+	layers.BaseLayer
+	Family uint16 // Address family (2 for IPv4)
+}
+
+func (h *WindowsLoopbackHeader) LayerType() gopacket.LayerType {
+	return layers.LayerTypeLoopback
+}
+
+func (h *WindowsLoopbackHeader) SerializeTo(b gopacket.SerializeBuffer, opts gopacket.SerializeOptions) error {
+	bytes, err := b.PrependBytes(4)
+	if err != nil {
+		return err
+	}
+
+	// Write address family in little-endian (Windows standard)
+	binary.LittleEndian.PutUint16(bytes[0:2], h.Family)
+	// Last 2 bytes are unused/reserved
+	binary.LittleEndian.PutUint16(bytes[2:4], 0)
+
+	return nil
+}
 
 // serializeLoopbackPacket handles Windows loopback packet serialization
-// Windows doesn't use any special header for loopback interfaces
 func serializeLoopbackPacket(buffer gopacket.SerializeBuffer, options gopacket.SerializeOptions, payload []byte) error {
+	header := &WindowsLoopbackHeader{
+		Family: 2, // AF_INET for IPv4
+	}
+
 	return gopacket.SerializeLayers(buffer, options,
+		header,
 		gopacket.Payload(payload))
 }
